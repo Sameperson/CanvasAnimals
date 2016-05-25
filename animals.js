93c7706c804fa56929fb89e92e10694a86092284
Saem <ottosigma@gmail.com>
2016-05-25 14:59:15 +0300

    ---

--- animals.js
+++ animals.js
@@ -6,52 +6,57 @@ function Animal() {
     this.y = Math.floor(Math.random() * (canvas.height - 40)) + 20;
     this.dy = Math.floor(Math.random() * 4) - 2;
     this.dx = Math.floor(Math.random() * 4) - 2;
-    this.letter = 'A';
-    this.circleColor = 'black';
-    this.draw = function () {
-        context.beginPath();
-        context.arc(this.x, this.y, 20, 0, 2 * Math.PI);
-        context.stroke();
-        context.fillStyle = this.circleColor;
-        context.fill();
-        context.fillStyle = 'white';
-        context.font = "30px Arial";
-        context.fillText(this.letter, this.x - 10, this.y + 10);
+}
+
+Animal.prototype.x = Math.floor(Math.random() * (canvas.width - 40)) + 20;
+Animal.prototype.y = Math.floor(Math.random() * (canvas.height - 40)) + 20;
+
+Animal.prototype.update = function () {
+    this.x += this.dx;
+    this.y += this.dy;
+    if(this.x > canvas.width - 20 || this.x < 20) {
+        this.dx = -this.dx;
     }
-    this.update = function () {
-        this.x += this.dx;
-        this.y += this.dy;
-        if(this.x > canvas.width - 20 || this.x < 20) {
-            this.dx = -this.dx;
-        }
-        if(this.y > canvas.height - 20 || this.y < 20) {
-            this.dy = -this.dy;
-        }
+    if(this.y > canvas.height - 20 || this.y < 20) {
+        this.dy = -this.dy;
     }
+}
+Animal.prototype.draw = function ()  {
+    context.beginPath();
+    context.arc(this.x, this.y, 20, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = this.circleColor;
+    context.fill();
+    context.fillStyle = 'white';
+    context.font = "30px Arial";
+    context.fillText(this.letter, this.x - 10, this.y + 10);
 }
 
 function Bird() {
-    this.t = Math.floor(Math.random() * 4);
     Animal.call(this);
-    this.letter = "B";
-    this.circleColor = "red";
-    this.update = function () {
-        this.t++;
-        this.y += Math.sin(this.t/30);
-    }
+    this.t = Math.floor(Math.random() * 4);
+}
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.letter = "B";
+Bird.prototype.circleColor = "red";
+Bird.prototype.update = function () {
+    this.t++;
+    this.y += Math.sin(this.t/30);
 }
 
 function Cat() {
     Animal.call(this);
-    this.letter = "C";
-    this.circleColor = "blue";
 }
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.letter = "C";
+Cat.prototype.circleColor = "blue";
 
 function Dog() {
     Animal.call(this);
-    this.letter = "D";
-    this.circleColor = "green";
 }
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.letter = "D";
+Dog.prototype.circleColor = "green";
 
 var bird1 = new Bird();
 var bird2 = new Bird();
